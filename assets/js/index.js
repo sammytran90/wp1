2c03f92c56eedc167f3b4132969b23a4fa102c45
TrungTA <trung.trananh0907@gmail.com>
2018-06-13 18:00:28 +0700

    commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,8 +2,21 @@ var app = new Vue({
     el: '#app',
     data: {
         slide: 1,
+        date: null,
         content: {
-            tittle: 'Meetup #2'
+            smalltittle: 'Legal Design Thinking',
+            tittle: 'Meetup #2',
+            topic: 'BURENRECHTER',
         }
+    },
+    methods: {
+        returnToday: function () {
+            var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+            var today = new Date();
+            return today.getDate() + ' ' + months[today.getMonth()] + ' ' + today.getFullYear();
+        }
+    },
+    created(){
+        this.date = this.returnToday();
     }
 })
